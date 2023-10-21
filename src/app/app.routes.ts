60104423f8dae83ec9e35f0e499742a901c6e75c
Sandra <sandra.gufler@gmx.ch>
2023-10-21 12:02:41 +0200

    Add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AddPlantComponent } from './containers/add-plant/add-plant.component';
+import { PlantInventoryComponent } from './containers/plant-inventory/plant-inventory.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'add-plant', component: AddPlantComponent },
+  { path: 'plant-inventory', component: PlantInventoryComponent },
+  { path: '', redirectTo: 'plant-inventory', pathMatch: 'full' }
+];
