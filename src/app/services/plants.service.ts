b1b6ab4d37bd48bab984901cd007b1affd0a4d12
Sandra <sandra.gufler@gmx.ch>
2023-10-21 11:57:42 +0200

    Create basic plants.service.ts

---
+++ src/app/services/plants.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, computed, signal, update } from '@angular/core';
+import { Plant } from '../types/plant.interface';
+import { isNil } from 'lodash-es';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlantsService {
+  LOCALE_STORAGE_KEY = 'plants'
+
+  state = signal<{ plants: Plant[] }>({ plants: this.getInitialState() });
+  plants = computed(() => this.state().plants);
+
+  getInitialState() {
+    const storedPlants = localStorage.getItem(this.LOCALE_STORAGE_KEY);
+    return !isNil(storedPlants) ? JSON.parse(storedPlants) : {};
+  }
+
+  saveToLocStor(plants: Plant[]): void {
+    if (isNil(plants)) {
+      localStorage.setItem(this.LOCALE_STORAGE_KEY, JSON.stringify(plants));
+    }
+  }
+
+  addPlant(plant: Plant): void {
+    if (!isNil(plant)) {
+      const updatedPlants = [...this.state().plants, plant];
+      this.state.update(currentState => ({ ...currentState, plants: updatedPlants }));
+      this.saveToLocStor(updatedPlants);
+    }
+  }
+
+  constructor() { }
+}
