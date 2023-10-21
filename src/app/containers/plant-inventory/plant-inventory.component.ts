255292bd68cd08406f7d137a549a7b9b200add1e
Sandra <sandra.gufler@gmx.ch>
2023-10-21 12:43:27 +0200

    Displaying plant cards works form loc stor

--- src/app/containers/plant-inventory/plant-inventory.component.ts
+++ src/app/containers/plant-inventory/plant-inventory.component.ts
@@ -1,12 +1,31 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderPlantInventoryComponent } from "../../components/header-plant-inventory/header-plant-inventory.component";
+import { PlantsService } from 'src/app/services/plants.service';
+import { Plant } from 'src/app/types/plant.interface';
+import { isNil } from 'lodash-es';
+import { PlantCardComponent } from "../../components/plant-card/plant-card.component";
 
 @Component({
   selector: 'plantino-plant-inventory',
   standalone: true,
   templateUrl: './plant-inventory.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule, HeaderPlantInventoryComponent]
+  imports: [CommonModule, HeaderPlantInventoryComponent, PlantCardComponent]
 })
-export class PlantInventoryComponent { }
+export class PlantInventoryComponent implements OnInit {
+  plants: Plant[] = [];
+
+  constructor(private plantsService: PlantsService) { }
+
+  ngOnInit() {
+    const plantsFromService = this.plantsService.plants();
+
+    if (isNil(plantsFromService) || !Array.isArray(plantsFromService)) {
+      this.plants = [];
+    } else {
+      this.plants = plantsFromService;
+    }
+  }
+
+}
