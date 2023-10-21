3b138af2d4e65d6f5719c1b20b42965dd8bf797f
Sandra <sandra.gufler@gmx.ch>
2023-10-21 13:02:56 +0200

    Started with form. Add buttons

--- src/app/containers/add-plant/add-plant.component.ts
+++ src/app/containers/add-plant/add-plant.component.ts
@@ -1,5 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 import { HeaderAddPlantComponent } from "../../components/header-add-plant/header-add-plant.component";
 
 @Component({
@@ -7,6 +10,12 @@ import { HeaderAddPlantComponent } from "../../components/header-add-plant/heade
   standalone: true,
   templateUrl: './add-plant.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule, HeaderAddPlantComponent]
+  imports: [CommonModule, HeaderAddPlantComponent, ReactiveFormsModule, MatButtonModule],
 })
-export class AddPlantComponent { }
+export class AddPlantComponent {
+  constructor(private router: Router) { }
+
+  navigateToPlantInventory() {
+    this.router.navigate(['plant-inventory']);
+  }
+}
